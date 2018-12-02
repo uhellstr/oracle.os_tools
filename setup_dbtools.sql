5b11e2189e4e1f0bb5a1a856e5104d3fe41672a8
Ulf Hellström <oraminute@gmail.com>
2018-12-02 14:14:02 +0100

    Minor fix

--- setup_dbtools.sql
+++ setup_dbtools.sql
@@ -6,7 +6,4 @@ REM
 @dbtools_directories.sql
 @setup_alrt_log_view.sql
 @dbtools_objs.sql
--- Setup APEX ws and APP for OS_TOOLS
-REM @remove_dbtools_ws.sql;
-REM @install_dbtools_ws.sql;
-REM @install_dbtools_app.sql;
+
