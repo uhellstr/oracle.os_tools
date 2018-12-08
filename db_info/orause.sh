55b6085e63a1f20bd3ef1d6516b21c2ca2a2f72f
Ulf Hellström <oraminute@gmail.com>
2018-12-08 16:55:36 +0100

    Adding DB_ABOUT table

--- db_info/orause.sh
+++ db_info/orause.sh
@@ -3,7 +3,7 @@ if [ -f ./cdb.log ] ; then
     rm ./cdb.log
 fi
 touch ./cdb.log
-ansible-playbook  -i ./hosts orause.yml -e ansible_ssh_port=22
+ansible-playbook  -i ./hosts orause.yml -e ansible_ssh_port=2222
 cat ./instance.log |sed 's/[][]//g' |sed 's/"//g' |sed 's/,/\n/g' |sed -e 's/^[ \t]*//' |sort |uniq >>./cdb.log
 rm ./instance.log
 ./db_info.py
--- db_info/setup_dbinfo_objs.sql
+++ db_info/setup_dbinfo_objs.sql
@@ -1,4 +1,5 @@
 @DB_INFO_TBL.sql
 @DB_ABOUT.sql
+@V_DB_INFO_V.sql
 @DB_INFO_PKS.sql
 @DB_INFO_PKB.sql
