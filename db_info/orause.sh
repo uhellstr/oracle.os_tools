dcc579c17d8ee8241d638c030e569815e7251fda
Ulf Hellstrom <ext.ulf.hellstrom@ehalsomyndigheten.se>
2018-12-13 16:11:02 +0100

    complete change of YML for Ansible that works in all env i tested it for.

--- db_info/orause.sh
+++ db_info/orause.sh
@@ -3,7 +3,12 @@ if [ -f ./cdb.log ] ; then
     rm ./cdb.log
 fi
 touch ./cdb.log
-ansible-playbook  -i ./hosts orause.yml -e ansible_ssh_port=2222
-cat ./instance.log |sed 's/[][]//g' |sed 's/"//g' |sed 's/,/\n/g' |sed -e 's/^[ \t]*//' |sort |uniq >>./cdb.log
+if [ -f ./instance.log ] ; then
+    rm ./instance.log
+fi
+ansible-playbook  -i ./hosts orause.yml -e ansible_ssh_port=22
+cat ./instance.log |sed -n -e 's/^.*\('stdout_lines'\)/\1/p' |cut -d "[" -f2 | cut -d "]" -f1  |sort |sed 's/,/\n/g' |sed -e 's/^[ \t]*//' |sed -e 's|["'\'']||g' >>./temp.log
+cat ./temp.log |sort |uniq >./cdb.log
 rm ./instance.log
-./db_info.py
+rm ./temp.log
+#./db_info.py
