c8d149fd286dc1fbd75fa6665b2cbf3f065fcce6
Ulf Hellström <oraminute@gmail.com>
2018-12-17 20:28:54 +0100

    Givint ansible part better name then orause so new name is collect.

--- db_info/collect.sh
+++ db_info/collect.sh
@@ -6,9 +6,9 @@ touch ./cdb.log
 if [ -f ./instance.log ] ; then
     rm ./instance.log
 fi
-ansible-playbook  -i ./hosts orause.yml -e ansible_ssh_port=22
+ansible-playbook  -i ./hosts collect.yml -e ansible_ssh_port=2222
 cat ./instance.log |sed -n -e 's/^.*\('stdout_lines'\)/\1/p' |cut -d "[" -f2 | cut -d "]" -f1  |sort |sed 's/,/\n/g' |sed -e 's/^[ \t]*//' |sed -e 's|["'\'']||g' >>./temp.log
 cat ./temp.log |sort |uniq >./cdb.log
 rm ./instance.log
 rm ./temp.log
-#./db_info.py
+./db_info.py
