755a5d28d830cf1cfadeda01b82c19e34cc7a86f
Ulf Hellstrom <oraminute@gmail.com>
2020-01-09 23:00:04 +0100

    New prep_appcon_pdb.sql example file

---
+++ autosetup/sql/autorun/appcon/prep_appcon_pdb.sql
@@ -0,0 +1,6 @@
+REM
+REM This is where you can put your own synchronizing for a PDB based on APPLICATIOM CONTAINER
+REM
+alter pluggable database application DB_APP sync;
+REM alter pluggable database application APEX sync;
+exit
