7fca0c545059bec817810ae0b86df15fc20ed697
Ulf Hellstrom <oraminute@gmail.com>
2020-09-01 11:00:49 +0200

    Complete new version of tnsgen

--- tnsgen/output.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-if [ -f ./cdb.log ] ; then
-    rm ./cdb.log
-fi
-cat ./instance.log |sed -n -e 's/^.*\('stdout_lines'\)/\1/p' |cut -d "[" -f2 | cut -d "]" -f1  |sort |sed 's/,/\n/g' |sed -e 's/^[ \t]*//' |sed -e 's|["'\'']||g' >>./temp.log
-cat ./temp.log |sort |uniq >./cdb.log
-rm ./instance.log
-rm ./temp.log
