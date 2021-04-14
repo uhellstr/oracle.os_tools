3a5199bf496f672737a5f06e89566432aff3c651
uhellstr <oraminute@gmail.com>
2021-04-14 15:32:40 +0200

    Cleaning up demo

---
+++ auditing/sql/DBAUDIT_SCHEMAS_PWD.sql
@@ -0,0 +1,6 @@
+REM
+REM change the passwords below
+REM
+alter user dbaudit_data identified by "xZsdfgh123lrtmnopiopzZ";
+alter user dbaudit_logik identified by "xZsdfgh123lrtmnopiopzZ";
+alter user dbaudit_access identified by "xZsdfgh123lrtmnopiopzZ";
---
+++ auditing/test/cleanup_schemas.sql
@@ -0,0 +1,3 @@
+drop user HR_ACCESS cascade;
+drop user HR_LOGIK cascade;
+drop user HR_DATA cascade;
